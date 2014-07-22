0527cdab034506343844cf13f2819fe088fd209b
larryhou <larryhou@foxmail.com>
2014-07-22 20:28:18 +0800

    update group rules

--- GroupSampleCode.sh
+++ GroupSampleCode.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd $(dirname $0)
 
-root=/Users/larryhou/Downloads/DEVSourceCode
+src=/Users/larryhou/Downloads/DEVSourceCode
+dir=/Users/larryhou/Documents/developer/samples
 
 group()
 {
@@ -22,24 +23,20 @@ group()
 		then
 			name=${line##*/}
 			
-			if [ -f $root/$name ]
+			if [ -f $src/$name ]
 			then
-				mv -fv $root/$name $folder
-			fi
-			
-			if [ -f $folder/$name ]
-			then
+				mv -fv $src/$name $folder
 				7z x -y $folder/$name -o$folder
 			fi
 		fi
 	done
 }
 
-group 1   65  $root/2014
-group 66  131 $root/2013
-group 132 150 $root/2012
-group 151 162 $root/2011
-group 163 177 $root/2010
-group 178 184 $root/2009
+group 1   65  $dir/2014
+group 66  131 $dir/2013
+group 132 150 $dir/2012
+group 151 162 $dir/2011
+group 163 177 $dir/2010
+group 178 184 $dir/2009
 
 
