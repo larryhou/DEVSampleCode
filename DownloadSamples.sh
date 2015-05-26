5ef5988c62115b1a0a3b78dd2cec53a9b9e1b4c7
larryhou <larryhou@foxmail.com>
2015-05-26 22:25:10 +0800

    Update

--- DownloadSamples.sh
+++ DownloadSamples.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-cat DEVSampleCode.txt | xargs -I{} wget -N {}
+
+output=/Users/larryhou/Downloads/DEVSourceCode
+while getopts :o:h OPTION
+do
+	case ${OPTION} in
+		o) output=${OPTARG};;
+		h) echo "Usage: $(basename $0) -o [OUTPUT_DIR] -h [HELP]"
+		   exit;;
+		:) echo "ERR: -${OPTARG} 缺少参数, 详情参考: $(basename $0) -h" 1>&2
+		   exit 1;;
+		?) echo "ERR: 输入参数-${OPTARG}不支持, 详情参考: $(basename $0) -h" 1>&2
+		   exit 1;;
+	esac
+done
+
+if [ ! -d "${output}" ]
+then
+	mkdir -pv ${output}
+fi
+
+cat DEVSampleCode.txt | xargs -I{} wget -N -P ${output} {}
